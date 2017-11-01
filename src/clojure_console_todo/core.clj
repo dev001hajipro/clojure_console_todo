460f09e46cd769ab8e0540648538e17f2407a9a8
dev001hajipro <dev001hajipro@gmail.com>
2017-11-01 21:44:53 +0900

    format and cleanup code.

--- src/clojure_console_todo/core.clj
+++ src/clojure_console_todo/core.clj
@@ -3,23 +3,11 @@
   (:gen-class))
 
 (def cli-options
-  ;; An option with a required argument
-  [["-p" "--port PORT" "Port number"
-    :default 80
-    :parse-fn #(Integer/parseInt %)
-    :validate [#(< 0 % 0x10000) "Must be a number between 0 and 65536"]]
-   ;; A non-idempotent option
-   ["-v" nil "Verbosity level"
-    :id :verbosity
-    :default 0
-    :assoc-fn (fn [m k _] (update-in m [k] inc))]
-   ;; A boolean option defaulting to nil
-   ["-h" "--help"]
+  [["-h" "--help"]
    ["-a" "--add" "Add todo"]
    ["-l" "--ls" "Print todo list"]
    ["-d" "--delete ID" "Delete todo by id"
-    :parse-fn #(Integer/parseInt %)]])
-   
+    :parse-fn #(Integer/parseInt %)]])   
 
 (defn -main [& args]
   ; todo create a data file unless it exist. 
@@ -41,11 +29,6 @@
       errors (do (print errors))
       :else (print summary))))
         
-        
-      
-
-  
-  
 
 (def cache-todo
   (atom {}))
